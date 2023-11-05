a847fc31e43b95f34ef65ae6301380a5b47ea828
Mokhirjon <Mokhirjon@test.com>
2023-11-05 20:56:01 +0500

    'changes'

--- University/models.py
+++ University/models.py
@@ -5,7 +5,7 @@ class UniversityModel(models.Model):
     name = models.CharField(max_length=50,default='')
     when_created = models.DateField(default=datetime.now)
     Who_created = models.CharField(max_length=50,default='')
-    photo = models.ImageField(upload_to='univesity_photos/')
+    photo = models.ImageField(upload_to='university_photos/')
     university_bio = models.TextField()
     location = models.CharField(max_length=50,default='')
     contact_with_admin = models.CharField(max_length=50,default='')
